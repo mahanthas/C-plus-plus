0d006298a94f112f67826d701576c4bc8f25ba96
mahantha <swamymahan280@gmail.com>
2025-02-10 08:40:07 +0530

    added const constexpr file

---
+++ const_constexpr.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+constexpr int square(int b){
+    return b*b;
+}
+int main(){
+    const int a = 10;
+    int value =  square(102);
+
+
+    cout<< a <<endl;
+    cout<<value<<endl;
+}
